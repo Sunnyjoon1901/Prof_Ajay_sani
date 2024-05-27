06a164bae3fbf11f3daf944cd380f1adea07518a
Sunnyjoon1901 <153222980+Sunnyjoon1901@users.noreply.github.com>
2024-05-27 11:29:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./home page";
 import Contact from "./contact page";
 import Students from "./students page";
@@ -7,19 +7,17 @@ import Projects from "./project page";
 import Academics from "./Academincs page";
 import PopularMedia from "./popular media page";
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/students" element={<Students />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/academic" element={<Academics />} />
-        <Route path="/popular-media" element={<PopularMedia/>} />
-        
+        <Route path="/popular-media" element={<PopularMedia />} />
       </Routes>
     </Router>
   );
